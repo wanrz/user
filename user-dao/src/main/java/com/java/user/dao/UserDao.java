08fa28c241da2d157e155c0b3cd6f14d5ec9d713
wanrz <723433727@qq.com>
2018-08-29 00:05:06 +0800

    111

---
+++ user-dao/src/main/java/com/java/user/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.java.user.dao;
+
+import com.java.user.entity.User;
+
+/**
+ * 用户DAO接口
+ * @author Administrator
+ *
+ */
+public interface UserDao {
+
+	public User login(User user);
+}
---
+++ user-service/src/main/java/com/java/user/service/UserService.java
@@ -0,0 +1,13 @@
+package com.java.user.service;
+
+import com.java.user.entity.User;
+
+/**
+ * 用户Service接口
+ * @author Administrator
+ *
+ */
+public interface UserService {
+
+	public User login(User user);
+}
